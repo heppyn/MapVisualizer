2bb9ee11a42b66be32fdabe1ef0f64501fa4b4de
Lukas Hepner <hepneluk@fit.cvut.cz>
2021-12-31 16:19:28 +0100

    Add background

--- lib/map-visualizer/visualizer.rb
+++ lib/map-visualizer/visualizer.rb
@@ -13,10 +13,11 @@ class Visualizer
 
   def trees(file_name)
     create_image
+    create_background(200)
 
     (0..@scene.width - 1).each do |x|
       (0..@scene.height - 1).each do |y|
-        @image[x, y] = ChunkyPNG::Color(@scene[x, y].tree ? Color::RGB::Red.hex : Color::RGB::Snow.hex)
+        @image[x, y] = ChunkyPNG::Color(Color::RGB::Red.hex) if @scene[x, y].tree
       end
     end
 
@@ -33,6 +34,18 @@ class Visualizer
     end
 
     save_image(file_name)
+  end
+
+  def create_background(alpha)
+    (0..@scene.width - 1).each do |x|
+      (0..@scene.height - 1).each do |y|
+        @image[x, y] = ChunkyPNG::Color.grayscale_alpha(
+          ChunkyPNG::Color.grayscale_teint(
+            Biome.biome_color(@scene[x, y].biome)
+          ), alpha
+        )
+      end
+    end
   end
 
   def create_image
