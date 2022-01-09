cd8b837ac0c74f0fde1b993e532c595902436547
Lukas Hepner <hepneluk@fit.cvut.cz>
2022-01-09 16:27:43 +0100

    Add generated maps

--- spec/visualizer_test.rb
+++ spec/visualizer_test.rb
@@ -9,18 +9,18 @@ describe Visualizer do
     scene = Loader.parse_scene('spec/test_data/export.json')
     visualizer = Visualizer.new(scene)
 
-    # it 'generates map with trees' do
-    #   visualizer.trees('./maps/trees.png')
-    # end
-    # it 'generates map with biomes' do
-    #   visualizer.biomes('./maps/biomes.png')
-    # end
-    # it 'generates map with humidity' do
-    #   visualizer.humidity('./maps/humidity.png')
-    # end
-    # it 'generates map with temperature' do
-    #   visualizer.temperature('./maps/temperature.png')
-    # end
+    it 'generates map with trees' do
+      visualizer.trees('./maps/trees.png')
+    end
+    it 'generates map with biomes' do
+      visualizer.biomes('./maps/biomes.png')
+    end
+    it 'generates map with humidity' do
+      visualizer.humidity('./maps/humidity.png')
+    end
+    it 'generates map with temperature' do
+      visualizer.temperature('./maps/temperature.png')
+    end
     it 'generates map with height' do
       visualizer.height('./maps/height.png')
     end
